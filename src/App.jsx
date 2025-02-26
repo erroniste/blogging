804b2dc8f4d5024024c9186d71d9bae8da73d38f
eayzex <exeayzex@gmail.com>
2025-02-26 19:15:13 +0100

    youssef

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-//import Header from './components/Header'
+import Header from './components/Header'
 import Hero from './components/Hero'
 //import BlogList from './components/BlogList'
 //import Footer from './components/Footer'
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div className="blog-container">
-      {/* <Header /> */}
+      <Header />
       <main className="blog-content">
         <Hero />
         {/* <BlogList posts={blogPosts} /> */}
